a6cacdaeac9c7c5a193159add30ad78cd3ca087e
Bane-999 <bane-93erep@hotmail.com>
2022-03-14 11:41:47 +0100

    Changed logic

--- linkedList.rb
+++ linkedList.rb
@@ -1,97 +1,124 @@
 class LinkedList
-    @list = []
+  @head
+  @tail
+  attr_reader :head, :tail
 
-    def initialize; end
-    
-    class Node
-        attr_accessor :value 
-        attr_accessor :next_node
+  def initialize; end
 
-        def initialize(value=nil, next_node=nil)
-            @value = value
-            @next_node = next_node
-        end       
+  class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value = nil, next_node = nil)
+      @value = value
+      @next_node = next_node
+    end
+  end
+
+  # def node_name(node)
+  #     a = node.inspect.split(" ")
+  #     a[0][14..]
+  # end
+
+  # adds a new node containing value to the end of the list
+  def append(value)
+    node = Node.new(value)
+    if (@head == nil)
+      @head = node
+      @tail = node
+    else
+      @tail.next_node = node
+      @tail = node
+    end
+  end
+
+  # adds a new node containing value to the start of the list
+  def prepend(value)
+    node = Node.new(value, @head)
+    if (@head == nil)
+      new_node = Node.new(value)
+      @head = new_node
+      @tail = new_node
+    else
+      @head = node
+    end
+  end
+
+  def size
+    count = 0
+    node = @head
+    until (node == nil)
+      node = node.next_node
+      count += 1
+    end
+    count
+  end
+
+  def at(index)
+    node = @head
+    index.times do
+      node = node.next_node
+      if (node == nil)
+        puts "No Node with this index!"
+        return
+      end
+    end
+    node
+  end
+
+  # removes the last element from the list
+  def pop
+    if (@head == nil)
+      puts "Linked List is empty!"
+      return
+    else
+      node = @head
+      until (node.next_node == tail)
+        node = node.next_node
+      end
+      pop_return = node.next_node
+      node.next_node = nil
+      @tail = node
+      pop_return
+    end
+  end
+
+  def contains?(value)
+    node = @head
+    until (node == nil)
+      if (value == node.value)
+        return true
+      else
+        node = node.next_node
+      end
+    end
+    return false
+  end
+
+  def find(value)
+    index = 0
+    node = @head
+    until (node == nil)
+      if (value == node.value)
+        return index
+      end
+
+      index += 1
+      node = node.next_node
+    end
+    return nil
+  end
+
+  def to_s
+    node = @head
+
+    if (node == nil)
+      puts "Linked List is empty!"
     end
 
-    def node_name(node)
-        a = node.inspect.split(" ")
-        a[0][14..]
+    until (node == nil)
+      print "( #{node.value} ) -> "
+      node = node.next_node
     end
-   
-
-    # adds a new node containing value to the end of the list
-    def append(value) 
-        node = Node.new(value)        
-        if(@list == nil)
-            @list = [node]  
-        else
-            @list.push(node) 
-            name = node_name(@list[-1])
-            @list[-2].next_node = name
-        end               
-    end
-
-    # adds a new node containing value to the start of the list
-    def prepend(value)  
-        node = Node.new(value,node_name(@list[0]))              
-        if(@list == nil)
-            @list = [Node.new(value)]  
-        else
-            @list.unshift(node)            
-        end   
-    end
-
-    def size
-        @list.size
-    end
-
-    def head
-        @list[0]
-    end
-
-    def tail
-        @list[-1]
-    end
-
-    def at(index)
-        @list[index]
-    end
-
-    # removes the last element from the list
-    def pop
-        if(@list.nil?)
-            puts "Linked List is empty!"
-            return
-        else
-            @list.pop
-            @list[-1].next_node = nil
-        end        
-    end
-
-    def contains?(value)
-        @list.any? {|x| x.value == value}
-    end
-
-    def find(value)  
-             
-        @list.each_with_index do |node,i|
-            if node.value == value
-                return i            
-            end
-        end
-        return nil
-    end
-
-    def to_s 
-        if @list == nil
-            puts "nil"
-        end
-        
-        @list.each do |node| 
-            print "( #{node.value} ) -> "
-            if (node.next_node == nil)
-                print "nil\n"
-            end
-        end
-    end
+    print "nil\n"
+  end
 end
